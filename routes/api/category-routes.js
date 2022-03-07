1923a117ed7daaa1f6f652e0d410efc382a64737
Expired <imoses2@hotmail.com>
2022-03-07 01:14:52 -0700

    create routes done

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,6 +43,12 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+	Category.create(req.body)
+	.then(data => res.status(200).json(data))
+	.catch(err => {
+		console.log(err);
+		res.status(500).json(err);
+	});
 });
 
 router.put('/:id', (req, res) => {
